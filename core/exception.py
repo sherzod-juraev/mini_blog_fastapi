dbd4144e044dd6fd0826127f136238de8316a994
JuraevSherzod <juraevsherzod9943@gmail.com>
2026-01-03 21:12:02 +0500

    Add exception handler function for FastAPI in core package

---
+++ core/exception.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError, ResponseValidationError
+from sqlalchemy.exc import TimeoutError
+
+
+def register_exception(app: FastAPI, /):
+
+
+    @app.exception_handler(RequestValidationError)
+    async def request_exception_handler(
+            request: Request,
+            exc: RequestValidationError
+    ):
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={'detail': 'Error in request'}
+        )
+
+
+    @app.exception_handler(ResponseValidationError)
+    async def response_exception_handler(
+            request: Request,
+            exc: ResponseValidationError
+    ):
+        return JSONResponse(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            content={'detail': 'Server response error'}
+        )
+
+
+    @app.exception_handler(TimeoutError)
+    async def timeout_exception_handler(
+            request: Request,
+            exc: TimeoutError
+    ):
+        return JSONResponse(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            content={
+                'detail': 'There are not enough server resources. Please try again later'
+            }
+        )
